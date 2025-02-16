384501f9630f3cc21025db51d1c8dabe19df655c
oresh247 <oresh247@gmail.com>
2025-02-16 13:08:06 +0300

    Добавлена возможность построения таблицы по всем сервисам

--- main.py
+++ main.py
@@ -22,8 +22,9 @@ sferaUrlLogin = config["SFERA"]["sferaUrlLogin"]
 sferaUrlGetFile = config["SFERA"]["sferaUrlGetFile"]
 sferaUrlKnowledge = config["SFERA"]["sferaUrlKnowledge"]
 sferaUrlKnowledge2 = config["SFERA"]["sferaUrlKnowledge2"]
+sferaUrlOrchestration = config["SFERA"]["sferaUrlOrchestration"]
 
-SERVICE_DIC = json.loads(config["ORCHESTRATION"]["SERVICE_DIC"])
+SERVICE_LST = json.loads(config["ORCHESTRATION"]["SERVICE_LST"])
 LIB_LST = json.loads(config["ORCHESTRATION"]["LIB_LST"])
 FILE_NAME = config["ORCHESTRATION"]["FILE_NAME"]
 
@@ -34,7 +35,8 @@ def get_file(project, build, file_name):
     url = sferaUrlGetFile + project + "/builds/" + build + "/downloadArtefact/?fullName=/SCA/" + file_name
     response = session.get(url, verify=False)
     if response.ok != True:
-        raise Exception("Error get sprint data " + response)
+        print("Error get file " + file_name)
+        return None
     return response.text
 
 
@@ -54,13 +56,16 @@ def get_builds(project):
         raise Exception("Error get sprint data " + response)
     return json.loads(response.text)
 
+
 def get_release_build_id(builds):
     # Перебираем содержимое сборок
+    if builds['content'] == None: return None, None
     for build in builds.get("content", []):
         display_name = build.get("displayName", "")
         if display_name.endswith("-release"):
             return build.get("id"), display_name
-    return None
+    return None, None
+
 
 def get_lib_version_list(gradle_dependency_tree):
     version_lst = []
@@ -92,16 +97,51 @@ def add_service_to_dataframe(df, service, release_build_name, lib_version_list):
     df.loc[service] = new_row
     return df
 
+
+def get_service_list():
+    url = sferaUrlOrchestration
+    response = session.get(url, verify=False)
+    if response.ok != True:
+        print("Error get service list")
+        return None
+    return json.loads(response.text)
+
+
+def get_all_service():
+    service_names = []
+    url = sferaUrlOrchestration
+    response = session.get(url, verify=False)
+    if response.ok != True: return None
+    for item in json.loads(response.text)["data"]:
+        if "name" in item:
+            service_names.append(item["name"])
+    return service_names
+
+
+def get_service_id(service, skmb_service_list):
+    for item in skmb_service_list["data"]:
+        if "name" in item and item["name"] == service:
+            return item["id"]
+
+    return None  # Сервис не найден
+
+
 def get_service_lib_versions():
     df = create_empty_dataframe()
-    for service, project in SERVICE_DIC.items():
+    skmb_service_list = get_service_list()
+    for service in SERVICE_LST:
+        project = get_service_id(service, skmb_service_list)
+        if project == None: continue
         # Вызываем функцию get_builds, передавая значение project
         builds = get_builds(project)
         release_build_id, release_build_name = get_release_build_id(builds)
+        if release_build_id == None: continue
         gradle_dependency_tree = get_file(project, str(release_build_id),FILE_NAME)
+        if gradle_dependency_tree == None: continue
         lib_version_list = get_lib_version_list(gradle_dependency_tree)
         df = add_service_to_dataframe(df, service, release_build_name, lib_version_list)
     return df
+
 
 def generate_release_html(df):
     # Генерируем HTML-код
@@ -110,18 +150,19 @@ def generate_release_html(df):
 
     # Декодируем HTML-спецсимволы
     decoded_html = html.unescape(html_code)
-    decoded_html = str.replace(decoded_html, '\\n', '')
-    decoded_html = str.replace(decoded_html, '\n', '')
-    decoded_html = str.replace(decoded_html, 'origin/', '')
     decoded_html = str.replace(decoded_html, '"', '')
-    decoded_html = str.replace(decoded_html, "'", '"')
+
+    # decoded_html = str.replace(decoded_html, '\\n', '')
+    # decoded_html = str.replace(decoded_html, '\n', '')
+    # decoded_html = str.replace(decoded_html, 'origin/', '')
+    # decoded_html = str.replace(decoded_html, "'", '"')
     # decoded_html = str.replace(decoded_html, 'class=sfera-link sfera-task sfera-link-style',
     #                            'class="sfera-link sfera-task sfera-link-style"')
     # decoded_html = str.replace(decoded_html, '<table border=1 class=dataframe>',
     #                            '<table class="MsoNormalTable" border="1" cellspacing="0" cellpadding="0" width="1440" data-widthmode="wide" data-lastwidth="1761px" style="border-collapse: collapse; width: 1761px;" id="mce_1">')
+
     decoded_html = str.replace(decoded_html, '<table border=1 class=dataframe>',
                                '<table border=1 style="border-collapse: collapse; width: 1800px;" id="mce_1-1723402032896-98" data-rtc-uid="244a0614-0d0b-42fd-b8af-5992e9fb70be">')
-
     return decoded_html
 
 
@@ -151,6 +192,15 @@ def generating_release_page(df, page_id, page_name):
     print(html)
     replace_release_html(html, page_id, page_name)
 
+page_id = "1490067"
+page_name = "ОКР.Библиотеки"
+ALL_SERVICE = False
+
+if ALL_SERVICE:
+    SERVICE_LST = get_all_service()
+    page_id = "1490081"
+    page_name = "ОКР.Библиотеки по всем сервисам"
+
 
 df = get_service_lib_versions()
-generating_release_page(df, "1490067", "ОКР.Библиотеки")
+generating_release_page(df, page_id, page_name)
